c41d7cd4a8571d6a4520f6fa1adc3b4f95db98ee
shreyash-01 <singhshreyash0075@gmail.com>
2023-11-10 15:53:04 +0530

    Created UserController and mapped /users and /users/{userID} urls

---
+++ src/main/java/com/example/demospringbootrest/users/UserController.java
@@ -0,0 +1,30 @@
+package com.example.demospringbootrest.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return this.userService.getUsers();
+    }
+
+    @GetMapping("/users/{userID}")
+    public User getUser(@PathVariable Integer userID){
+        return userService.getUser(userID);
+    }
+
+}
