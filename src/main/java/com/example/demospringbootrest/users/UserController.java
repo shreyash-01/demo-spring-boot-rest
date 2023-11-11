631a5200e9b577d82026bfd7abd42a7c9da2567b
shreyash-01 <singhshreyash0075@gmail.com>
2023-11-11 09:27:57 +0530

    Added a mapping for POST request for creating a user

--- src/main/java/com/example/demospringbootrest/users/UserController.java
+++ src/main/java/com/example/demospringbootrest/users/UserController.java
@@ -1,14 +1,12 @@
 package com.example.demospringbootrest.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     private UserService userService;
 
@@ -17,14 +15,19 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping("/")
     public List<User> getAllUsers(){
         return this.userService.getUsers();
     }
 
-    @GetMapping("/users/{userID}")
+    @GetMapping("/{userID}")
     public User getUser(@PathVariable Integer userID){
         return userService.getUser(userID);
+    }
+
+    @PostMapping("/")
+    public String createUser(@RequestBody User user){
+        return userService.createNewUser(user);
     }
 
 }
